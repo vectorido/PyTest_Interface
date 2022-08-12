388c9afe21e63ee039422127d3d84f460defc30e
Ilia Chernetsov <93093814+vectorido@users.noreply.github.com>
2022-08-12 20:15:48 +0400

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -5,6 +5,4 @@ def test_assert_button_presence(browser):
     browser.get(f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     browser.implicitly_wait(10)
 
-    basket_button = browser.find_element(By.CSS_SELECTOR, '.btn.btn-lg.btn-add-to-basket')
-
-    assert basket_button
+    assert browser.find_element(By.CSS_SELECTOR, '.btn.btn-lg.btn-add-to-basket')
